aee18579d651c98d65cf0bf82fc17276fc48fc06
Ethan <ethansmithson413@gmail.com>
2024-06-20 19:06:22 -0400

    API Integration

--- myApp/urls.py
+++ myApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('tickets/', views.tickets, name='tickets'),
+    # path('findTickets/', views.findTickets, name='findTickets'),
     path('home/', views.home, name='home'),
     path('explore/', views.explore, name='explore'),
     path('register/', views.register, name='register'),
@@ -11,4 +12,7 @@ urlpatterns = [
     path('check_username/', views.check_username, name='check_username'),
     path('check_email/', views.check_email, name='check_email'),
     path('activate-user/<uidb64>/<token>', views.activate_user, name='activate'),
+    path('origin_airport_search/', views.origin_airport_search, name='origin_airport_search'),
+    path('destination_airport_search/', views.destination_airport_search, name='destination_airport_search'),
+    path('book_flight/<str:flight>/', views.book_flight, name='book_flight')
 ]
